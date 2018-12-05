5eccde8852c290515947b878466cb50198dc2de8
Taylor Savage <tcsjazz@gmail.com>
2018-12-05 10:57:53 -0600

    Audio button creation

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-/**function playSound(e) {
-	let audioNumber = e.keyCode;
-  	const audio = document.querySelector(`audio[data-key="${audioNumber}"]`);
-  	if (!audio) return;
-  	audio.currentTime = 0;
-  	audio.play();
-  	button.classList.toggle('playing');
-}**/
-let soundOne = document.querySelector('#sound1');
+function toggleClass(e) {
+	if (e.target !== e.currentTarget) {
+		let clicked = e.target;
+		clicked.classList.toggle('playing');
+		console.log(clicked);
 
-soundOne.addEventListener('click', toggleClass, false);
+	}
+}
 
-function toggleClass() {
-  soundOne.classList.toggle('playing');
+function playSound(e) {
+	let clicked = e.target;
+	let audioNumber = e.target.id;
+	const audio = document.querySelector(`audio[data-button="${audioNumber}"]`);
+	if (!audio) return;
+	if (audio.paused) {
+		audio.currentTime = 0;
+		audio.play();
+		clicked.innerHTML = 'Stop';
+	} else {
+		audio.pause();
+		clicked.innerHTML = `Play Clip #${e.target.id}`;
+	}	
 }
 
 
-
+var theMaster = document.getElementById("master");
+theMaster.addEventListener("mousedown", toggleClass, false);
+theMaster.addEventListener("mouseup", toggleClass, false);
+theMaster.addEventListener("click", playSound, false);
