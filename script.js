facc803009f04b2eecf521e1b68d26c42c04b6e3
Taylor Savage <tcsjazz@gmail.com>
2018-12-05 12:26:18 -0600

    Audio tracks and button design

--- script.js
+++ script.js
@@ -3,27 +3,30 @@ function toggleClass(e) {
 		let clicked = e.target;
 		clicked.classList.toggle('playing');
 		console.log(clicked);
-
 	}
+	e.stopPropagation();
 }
 
 function playSound(e) {
-	let clicked = e.target;
+	let clickedButton = e.target;
 	let audioNumber = e.target.id;
 	const audio = document.querySelector(`audio[data-button="${audioNumber}"]`);
 	if (!audio) return;
 	if (audio.paused) {
 		audio.currentTime = 0;
 		audio.play();
-		clicked.innerHTML = 'Stop';
+		clickedButton.innerHTML = 'Stop';
 	} else {
 		audio.pause();
-		clicked.innerHTML = `Play Clip #${e.target.id}`;
-	}	
+		clickedButton.innerHTML = `Play Clip #${e.target.id}`;
+	}
+	e.stopPropagation();
 }
 
-
 var theMaster = document.getElementById("master");
+
 theMaster.addEventListener("mousedown", toggleClass, false);
+
 theMaster.addEventListener("mouseup", toggleClass, false);
+
 theMaster.addEventListener("click", playSound, false);
